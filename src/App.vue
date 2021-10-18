d6ea0cba992cba6fe308f64ce8a9e8b8c58a20a3
jiyuujin <jiyuujinunite@gmail.com>
2021-10-18 10:42:07 +0900

    feat: alert, others
    - show alert
    - reference to repositories

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <main>
     <h1>Spaces in Routine</h1>
+    <alert />
     <issues />
     <footer-text />
   </main>
@@ -9,11 +10,13 @@
 <script lang="ts">
 import Issues from './components/Issues.vue'
 import FooterText from './components/FooterText.vue'
+import Alert from './components/Alert.vue'
 
 export default {
   components: {
     Issues,
-    FooterText
+    FooterText,
+    Alert
   }
 }
 </script>
