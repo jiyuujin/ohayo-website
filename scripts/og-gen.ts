34aeb936c5f04b8bd45039835c46f7a6dfa338b6
jiyuujin <jiyuujinunite@gmail.com>
2021-10-24 08:07:34 +0900

    feat: og meta - resolved #5

---
+++ scripts/og-gen.ts
@@ -0,0 +1,40 @@
+'use strict'
+
+const puppeteer = require('puppeteer')
+const path = require('path')
+
+const fs = require('fs')
+
+const OG_SOURCE_DIR_PATH = path.join(__dirname, '..', 'data')
+const OG_SOURCE_HTML_FILE_PATH = path.join(OG_SOURCE_DIR_PATH, 'og.html')
+const OG_DIR_PATH = path.join(__dirname, '..', 'public', 'og')
+
+async function captureOgImage(browser: any, title: string, description: string, imagePath: string) {
+  const page = await browser.newPage()
+  await page.setViewport({
+    width: 1200,
+    height: 630,
+  })
+  await page.goto('file://' + OG_SOURCE_HTML_FILE_PATH)
+  await page.exposeFunction('getTitle', () => title)
+  await page.exposeFunction('getDescription', () => description)
+  await page.reload()
+  await page.screenshot({
+    path: imagePath,
+    type: 'jpeg',
+  })
+  await page.close()
+}
+
+;(async function () {
+  const ogList = [{ title: 'おはようエンジニア', description: '#ohayo_engineer', slug: 'ohayo-engineer' }]
+
+  const browser = await puppeteer.launch()
+
+  for (const { title, description, slug } of ogList) {
+    const OG_IMAGE_FILE_PATH = `${OG_DIR_PATH}/` + slug + '.jpg'
+    await captureOgImage(browser, title, description, OG_IMAGE_FILE_PATH)
+  }
+
+  await browser.close()
+})()
--- src/App.vue
+++ src/App.vue
@@ -13,9 +13,73 @@ import FooterText from './components/FooterText.vue'
 import Alert from './components/Alert.vue'
 
 useHead({
-  title: 'Vitesse',
+  title: 'おはようエンジニア',
   meta: [
-    { name: 'description', content: 'Opinionated Vite Starter Template' },
-  ],
+    {
+      hid: 'description',
+      name: 'description',
+      content: '平日は毎日、Twitter Spaces でお喋りしている内容のメモを記録しています。'
+    },
+    {
+      hid: 'og:site_name',
+      name: 'og:site_name',
+      content: 'おはようエンジニア'
+    },
+    {
+      hid: 'og:type',
+      name: 'og:type',
+      content: 'website'
+    },
+    {
+      hid: 'og:description',
+      name: 'og:description',
+      content: '平日は毎日、Twitter Spaces でお喋りしている内容のメモを記録しています。'
+    },
+    {
+      hid: 'og:title',
+      name: 'og:title',
+      content: 'おはようエンジニア'
+    },
+    {
+      hid: 'og:url',
+      name: 'og:url',
+      content: 'https://ohayo.nekohack.me/'
+    },
+    {
+      hid: 'twitter:description',
+      name: 'twitter:description',
+      content: '平日は毎日、Twitter Spaces でお喋りしている内容のメモを記録しています。'
+    },
+    {
+      hid: 'twitter:title',
+      name: 'twitter:title',
+      content: 'おはようエンジニア'
+    },
+    {
+      hid: 'twitter:card',
+      name: 'twitter:card',
+      content: 'summary_large_image' // ex: summary, summary_large_image
+    },
+    {
+      hid: 'twitter:site',
+      name: 'twitter:site',
+      content: '@jiyuujinlab'
+    },
+    {
+      hid: 'twitter:creator',
+      name: 'twitter:creator',
+      content: '@jiyuujinlab'
+    },
+    {
+      hid: 'twitter:site',
+      name: 'twitter:site',
+      content: '@jiyuujinlab'
+    },
+    {
+      hid: 'twitter:image',
+      name: 'twitter:image',
+      content: 'https://ohayo.nekohack.me/og/ohayo-engineer.jpg',
+    }
+  ]
 })
 </script>
