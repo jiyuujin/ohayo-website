cdea8479115960846223fe21c9944bcbeeb5c466
jiyuujin <jiyuujinunite@gmail.com>
2021-11-10 14:05:48 +0900

    update title

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,7 +6,9 @@ import Alert from '../components/Alert.vue'
 
 <template>
   <main>
-    <h1>Spaces in Routine</h1>
+    <h1>{{ `みんなで作るブログ` }}</h1>
+    <h2>{{ `#ohayo_engineer` }}</h2>
+    <p>{{ `平日は毎日、Twitter Spaces でお喋りしている内容のメモを記録しています。` }}</p>
     <alert />
     <issues />
     <footer-text />
