9c1ac718d748c22eb626bd4cbb7e8627f2499d88
jiyuujin <jiyuujinunite@gmail.com>
2021-11-15 08:38:38 +0900

    refactor: tweet button

---
+++ src/components/TweetButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <a
+    href="https://twitter.com/intent/tweet?hashtags=ohayo_engineer"
+    target="_blank"
+    rel="noopener noreferrer"
+    class="tweet_button"
+    role="button"
+  >
+    {{ `#ohayo_engineer` }}
+    <span class="icon_wrapper">
+      <TwitterIcon />
+    </span>
+  </a>
+  <p>{{ `平日は毎日、Twitter Spaces でお喋りしている内容のメモを記録しています。` }}</p>
+</template>
+
+<script lang="ts">
+import TwitterIcon from '../assets/twitter.svg'
+
+export default {
+  components: {
+    TwitterIcon
+  }
+}
+</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,13 +2,13 @@
 import Issues from '../components/Issues.vue'
 import FooterText from '../components/FooterText.vue'
 import Alert from '../components/Alert.vue'
+import TweetButton from '../components/TweetButton.vue'
 </script>
 
 <template>
   <main>
     <h1>{{ `みんなで作るブログ` }}</h1>
-    <h2>{{ `#ohayo_engineer` }}</h2>
-    <p>{{ `平日は毎日、Twitter Spaces でお喋りしている内容のメモを記録しています。` }}</p>
+    <tweet-button />
     <alert />
     <issues />
     <footer-text />
