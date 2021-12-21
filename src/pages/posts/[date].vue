dfe98e9ff07c83d86cbde28561b8a047b8aef993
jiyuujin <jiyuujinunite@gmail.com>
2021-12-21 21:26:17 +0900

    delete: post og

--- src/pages/posts/[date].vue
+++ src/pages/posts/[date].vue
@@ -8,64 +8,6 @@ import { currentDateText } from '../../services/utilService'
 const props = defineProps<{ date: string }>()
 const postDate = computed(() => props.date)
 const postDateText = computed(() => currentDateText(props.date))
-
-useHead({
-  title: `おはようエンジニア in ${postDateText}`,
-  meta: [
-    {
-      name: 'description',
-      content: `${postDateText} に Twitter Spaces でお喋りした内容のメモを記録しています。`
-    },
-    {
-      name: 'og:site_name',
-      content: `おはようエンジニア in ${postDateText}`
-    },
-    {
-      name: 'og:type',
-      content: 'website'
-    },
-    {
-      name: 'og:description',
-      content: `${postDateText} に Twitter Spaces でお喋りした内容のメモを記録しています。`
-    },
-    {
-      name: 'og:title',
-      content: `おはようエンジニア in ${postDateText}`
-    },
-    {
-      name: 'og:url',
-      content: 'https://ohayo.nekohack.me/'
-    },
-    {
-      name: 'twitter:description',
-      content: `${postDateText} に Twitter Spaces でお喋りした内容のメモを記録しています。`
-    },
-    {
-      name: 'twitter:title',
-      content: `おはようエンジニア in ${postDateText}`
-    },
-    {
-      name: 'twitter:card',
-      content: 'summary_large_image' // ex: summary, summary_large_image
-    },
-    {
-      name: 'twitter:site',
-      content: '@jiyuujinlab'
-    },
-    {
-      name: 'twitter:creator',
-      content: '@jiyuujinlab'
-    },
-    {
-      name: 'twitter:site',
-      content: '@jiyuujinlab'
-    },
-    {
-      name: 'twitter:image',
-      content: 'https://ohayo.nekohack.me/og/ohayo-engineer.jpg',
-    }
-  ]
-})
 </script>
 
 <template>
