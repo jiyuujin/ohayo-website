906d6659521a2838ceb933f88cd5369ccc772f37
jiyuujin <jiyuujinunite@gmail.com>
2021-12-23 13:51:08 +0900

    fix: lint

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
   <h1>{{ msg }}</h1>
-  <button @click="() => count++">count is: {{ count }}</button>
+  <button @click="() => count++">
+    count is: {{ count }}
+  </button>
   <p>
     Edit <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
