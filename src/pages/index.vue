92ff300e91b881a3012419d1b2037230a35edce8
jiyuujin <jiyuujinunite@gmail.com>
2022-02-11 22:39:24 +0900

    refactor: og meta, others
    - update vite plugins

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,6 +3,64 @@ import IssuesView from '../components/IssuesView.vue'
 import FooterText from '../components/FooterText.vue'
 import AlertView from '../components/AlertView.vue'
 import TweetButton from '../components/TweetButton.vue'
+
+useHead({
+  title: 'おはようエンジニア',
+  meta: [
+    {
+      name: 'description',
+      content: '平日毎朝、Twitter Spaces でお喋りしている内容のメモを記録しています。'
+    },
+    {
+      name: 'og:site_name',
+      content: 'おはようエンジニア'
+    },
+    {
+      name: 'og:type',
+      content: 'website'
+    },
+    {
+      name: 'og:description',
+      content: '平日は毎日、Twitter Spaces でお喋りしている内容のメモを記録しています。'
+    },
+    {
+      name: 'og:title',
+      content: 'おはようエンジニア'
+    },
+    {
+      name: 'og:url',
+      content: 'https://ohayo.nekohack.me/'
+    },
+    {
+      name: 'twitter:description',
+      content: '平日は毎日、Twitter Spaces でお喋りしている内容のメモを記録しています。'
+    },
+    {
+      name: 'twitter:title',
+      content: 'おはようエンジニア'
+    },
+    {
+      name: 'twitter:card',
+      content: 'summary_large_image' // ex: summary, summary_large_image
+    },
+    {
+      name: 'twitter:site',
+      content: '@jiyuujinlab'
+    },
+    {
+      name: 'twitter:creator',
+      content: '@jiyuujinlab'
+    },
+    {
+      name: 'twitter:site',
+      content: '@jiyuujinlab'
+    },
+    {
+      name: 'twitter:image',
+      content: 'https://ohayo.nekohack.me/og/ohayo-engineer.jpg',
+    }
+  ]
+})
 </script>
 
 <template>
--- src/pages/posts/[date].vue
+++ src/pages/posts/[date].vue
@@ -8,6 +8,64 @@ import { currentDateLabelText } from '../../services/utilService'
 const props = defineProps<{ date: string }>()
 const postDate = computed(() => props.date)
 const postDateLabel = computed(() => currentDateLabelText(props.date))
+
+useHead({
+  title: `${postDateLabel.value}の雑談記録`,
+  meta: [
+    {
+      name: 'description',
+      content: `${postDateLabel.value}に Twitter Spaces でお喋りした内容のメモを記録しています。`
+    },
+    {
+      name: 'og:site_name',
+      content: `${postDateLabel.value}の雑談記録`
+    },
+    {
+      name: 'og:type',
+      content: 'website'
+    },
+    {
+      name: 'og:description',
+      content: `${postDateLabel.value}に Twitter Spaces でお喋りした内容のメモを記録しています。`
+    },
+    {
+      name: 'og:title',
+      content: `${postDateLabel.value}の雑談記録`
+    },
+    {
+      name: 'og:url',
+      content: `https://ohayo.nekohack.me/posts/${postDate.value}`
+    },
+    {
+      name: 'twitter:description',
+      content: `${postDateLabel.value}に Twitter Spaces でお喋りした内容のメモを記録しています。`
+    },
+    {
+      name: 'twitter:title',
+      content: `${postDateLabel.value}の雑談記録`
+    },
+    {
+      name: 'twitter:card',
+      content: 'summary_large_image' // ex: summary, summary_large_image
+    },
+    {
+      name: 'twitter:site',
+      content: '@jiyuujinlab'
+    },
+    {
+      name: 'twitter:creator',
+      content: '@jiyuujinlab'
+    },
+    {
+      name: 'twitter:site',
+      content: '@jiyuujinlab'
+    },
+    {
+      name: 'twitter:image',
+      content: 'https://ohayo.nekohack.me/og/ohayo-engineer.jpg',
+    }
+  ]
+})
 </script>
 
 <template>
