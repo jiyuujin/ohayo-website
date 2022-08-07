69e0bc8a7b915ca921b58f12942e753b0e21001f
jiyuujin <jiyuujinunite@gmail.com>
2022-08-08 08:02:55 +0900

    update logo, others

---
+++ src/utils/constants.ts
@@ -0,0 +1,8 @@
+import Endpoints from './endpoints.constants'
+
+export const SOCIAL_LIST = ['twitter', 'hatena', 'note', 'github']
+export const TWITTER_URL = (title: string, slug: string) =>
+  `http://twitter.com/share?text=${title}&url=${Endpoints.BASE_URL}posts/${slug}`
+export const HATENA_URL = (slug: string) => `http://b.hatena.ne.jp/entry/webneko.dev/posts/${slug}`
+export const NOTE_URL = (slug: string) =>
+  `https://note.mu/intent/post?url=${Endpoints.BASE_URL}posts/${slug}`
