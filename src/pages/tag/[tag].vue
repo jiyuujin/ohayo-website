61c80c6f288844fe6f2e027c372487aa2699e6a6
jiyuujin <jiyuujinunite@gmail.com>
2023-01-06 13:49:21 +0900

    remove alert view

--- src/pages/tag/[tag].vue
+++ src/pages/tag/[tag].vue
@@ -2,7 +2,6 @@
 import ArchivesView from '../../components/ArchivesView.vue'
 import FooterText from '../../components/FooterText.vue'
 import NavText from '../../components/NavText.vue'
-import AlertView from '../../components/AlertView.vue'
 
 const props = defineProps<{ tag: string }>()
 const postTag = computed(() => props.tag)
@@ -11,7 +10,6 @@ const postTag = computed(() => props.tag)
 <template>
   <main>
     <nav-text />
-    <alert-view />
     <archives-view :tag="postTag" />
     <footer-text />
   </main>
