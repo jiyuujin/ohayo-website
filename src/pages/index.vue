5e23d3deafc946e2a6ce6fcff42b29d88e0fc64c
jiyuujin <jiyuujinunite@gmail.com>
2023-01-06 13:47:40 +0900

    remove alert view

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,13 +3,11 @@ import ArchivesView from '../components/ArchivesView.vue'
 import FooterText from '../components/FooterText.vue'
 import NavText from '../components/NavText.vue'
 import MaintainerView from '../components/MaintainerView.vue'
-import AlertView from '../components/AlertView.vue'
 </script>
 
 <template>
   <main>
     <nav-text />
-    <alert-view />
     <archives-view />
     <maintainer-view />
     <footer-text />
--- src/pages/posts/[date].vue
+++ src/pages/posts/[date].vue
@@ -2,7 +2,6 @@
 import IssueView from '../../components/IssueView.vue'
 import FooterText from '../../components/FooterText.vue'
 import NavText from '../../components/NavText.vue'
-import AlertView from '../../components/AlertView.vue'
 
 import { currentDateLabelText } from '../../services/utilService'
 
@@ -14,7 +13,6 @@ const postDateLabel = computed(() => currentDateLabelText(props.date))
 <template>
   <main>
     <nav-text />
-    <alert-view />
     <issue-view :date="postDate" />
     <footer-text />
   </main>
