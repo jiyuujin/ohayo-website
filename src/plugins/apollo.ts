2272cd782ee69ce4e012cb160574293dfd424dac
yuma-kitamura <jiyuujinunite@gmail.com>
2023-02-23 13:01:16 +0900

    chore: apollo composable to beta

--- src/plugins/apollo.ts
+++ src/plugins/apollo.ts
@@ -1,15 +1,21 @@
 import fetch from 'isomorphic-unfetch'
-import ApolloClient from 'apollo-client'
-import { HttpLink } from 'apollo-link-http'
-import { ApolloLink, concat } from 'apollo-link'
-import { InMemoryCache, IntrospectionFragmentMatcher } from 'apollo-cache-inmemory'
-import introspectionQueryResultData from './fragmentTypes.json'
+import { ApolloClient, ApolloLink, concat, HttpLink, InMemoryCache, PossibleTypesMap } from '@apollo/client/core'
+import QueryResult from './fragmentTypes.json'
 
 const GITHUB_API_V4 = 'https://api.github.com/graphql'
 
-const fragmentMatcher = new IntrospectionFragmentMatcher({
-  introspectionQueryResultData
-})
+// https://github.com/apollographql/apollo-client/issues/6855#issuecomment-678822460
+const introspectionToPossibleTypes = (data): PossibleTypesMap => {
+  const possibleTypes = {}
+
+  data.__schema.types.forEach((supertype) => {
+    if (supertype.possibleTypes) {
+      possibleTypes[supertype.name] = supertype.possibleTypes.map((subtype) => subtype.name)
+    }
+  })
+
+  return possibleTypes
+}
 
 const authMiddleware = new ApolloLink((operation, forward) => {
   operation.setContext({
@@ -31,6 +37,6 @@ const httpLink = new HttpLink({
 export const apolloClient = new ApolloClient({
   link: concat(authMiddleware, httpLink),
   cache: new InMemoryCache({
-    fragmentMatcher
+    possibleTypes: introspectionToPossibleTypes(QueryResult),
   })
 })
